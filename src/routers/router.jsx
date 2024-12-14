3c309158f542f37556253a323ee2f39ebd353b31
dhinesh0710 <87359826+dhinesh0710@users.noreply.github.com>
2024-12-14 18:11:54 +0530

    Updated router.jsx import

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/home/home";
+import Home from "../pages/home/home.jsx";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
